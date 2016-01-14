fd9655dcc24ba71a47c6199b18f5bb640e1b52ee
Alvaro Massana <alvaromassana@gmail.com>
2016-01-14 11:55:48 +0100

    Day 4 first exercise

---
+++ Day 4/class.rb
@@ -0,0 +1,58 @@
+class User
+
+	def initialize
+		puts "Username:"
+
+		user_name = gets.chomp
+
+		if user_name == "alvaro"
+
+			puts "OK!"
+			password(user_name)
+
+		else
+			puts "try again please"
+			username
+		end
+	end
+
+	def password(user_name)
+		puts "Password:"
+
+		password = gets.chomp
+
+		if password == "massana"
+			puts "you are in!"
+
+			Text.new
+
+		else
+
+			puts "Try again!"
+
+			password(username)
+		end	
+
+	end
+
+end
+
+class Text
+	def initialize
+		puts "write text:"
+
+		text = gets.chomp
+
+		count_words(text)
+
+	end
+
+	def count_words(text)
+		counter = text.split(' ').length
+		puts counter
+
+	end
+end
+
+
+User.new
