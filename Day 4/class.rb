cd997908333d4b1cf787d89ec0cdbfb75584ca6a
Alvaro Massana <alvaromassana@gmail.com>
2016-01-14 12:03:59 +0100

    Day 4 first exercise v- 2

--- Day 4/class.rb
+++ Day 4/class.rb
@@ -1,15 +1,10 @@
 class User
-
 	def initialize
 		puts "Username:"
-
 		user_name = gets.chomp
-
 		if user_name == "alvaro"
-
 			puts "OK!"
 			password(user_name)
-
 		else
 			puts "try again please"
 			username
@@ -18,41 +13,28 @@ class User
 
 	def password(user_name)
 		puts "Password:"
-
 		password = gets.chomp
-
 		if password == "massana"
 			puts "you are in!"
-
 			Text.new
-
 		else
-
 			puts "Try again!"
-
 			password(username)
 		end	
-
 	end
-
 end
 
 class Text
 	def initialize
 		puts "write text:"
-
 		text = gets.chomp
-
 		count_words(text)
-
 	end
 
 	def count_words(text)
 		counter = text.split(' ').length
-		puts counter
-
+		puts "Text has #{counter} words" 
 	end
 end
-
 
 User.new
